566bd4a24803c6b030be5f89dee1a3bec7295c82
Piotr Romaniuk <piotr.romaniuk@elesoftrom.com.pl>
2023-09-06 15:34:32 +0200

    oznaczenia praw autorskich w skompilowanym kodzie

---
+++ CPU/cpu01/Software/copyright_lopi_visible.cpp
@@ -0,0 +1,17 @@
+/*
+ * copyright_lopi_visible.cpp
+ *
+ *  Created on: 28 paü 2022
+ *      Author: Piotr
+ */
+#include <stdint.h>
+
+#pragma RETAIN
+
+const uint16_t  lopi_copyright[] = {
+             '2','0','1','9',' ','(','c',')',' ',
+             'L','O','P','I','-','E','l','e','k','t','r','o','n','i','k','a'
+};
+
+
+
