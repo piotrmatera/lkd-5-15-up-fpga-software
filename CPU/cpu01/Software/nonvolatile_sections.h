7bc50a892b9fa2e138f154c492ca64a4c7c1185c
Piotr Romaniuk <piotr.romaniuk@elesoftrom.com.pl>
2023-10-17 13:09:25 +0200

    wylaczenie timeoutow bo nie dzialaly - zawieszalo sie przy starcie

--- CPU/cpu01/Software/nonvolatile_sections.h
+++ CPU/cpu01/Software/nonvolatile_sections.h
@@ -14,6 +14,7 @@
 #define NV_LGRID_TYPE 1
 #define NV_ERROR_RETRY_TYPE 2
 
+#if 0
 #define NV_ONOFF_SAVE_TIMEOUT 40 //ms
 #define NV_LGRID_SAVE_TIMEOUT 100 //ms
 #define NV_ERROR_RETRY_SAVE_TIMEOUT 40 //ms
@@ -21,8 +22,15 @@
 #define NV_ONOFF_READ_TIMEOUT 10 //ms
 #define NV_LGRID_READ_TIMEOUT 20 //ms
 #define NV_RETRY_READ_TIMEOUT_ERROR 10 //ms
+#endif
 
+#define NV_ONOFF_SAVE_TIMEOUT 0 //ms
+#define NV_LGRID_SAVE_TIMEOUT 0 //ms
+#define NV_ERROR_RETRY_SAVE_TIMEOUT 0 //ms
 
+#define NV_ONOFF_READ_TIMEOUT 0 //ms
+#define NV_LGRID_READ_TIMEOUT 0 //ms
+#define NV_RETRY_READ_TIMEOUT_ERROR 0 //ms
 
 
 
